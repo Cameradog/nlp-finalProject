92a0e1e4ccb725dd3e2abc29f723a176600a3b22
HanVincent <b84414@gmail.com>
2015-11-17 11:18:01 +0800

    Tokenize, Stem, Lemma.

--- src/feature/Tokenization.java
+++ src/feature/Tokenization.java
@@ -1,5 +1,22 @@
 package feature;
+import edu.stanford.nlp.process.*;
 
 public class Tokenization {
 
+	//get twitter content then split.
+	String line = "Hello, There are many apples.";
+	String content = line;
+	content = content.split(",")[3];
+	String[] content_token = content.split(" ");
+	
+	for(String e1: content_token){
+		String stemming = new Morphology().stem(e1);
+	}
+	
+	for(String e2: content_token){
+		String lemma = new Morphology().lemma(word, tag);
+	}
+	
+	
+}
 }
