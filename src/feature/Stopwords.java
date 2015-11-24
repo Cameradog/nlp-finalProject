4242c6c2e6bc247ff83121654eba7d581eef45b1
HanVincent <b84414@gmail.com>
2015-11-24 14:23:56 +0800

    Stem and Stopword test done.

--- src/feature/Stopwords.java
+++ src/feature/Stopwords.java
@@ -6,8 +6,14 @@ import java.io.IOException;
 import java.util.Hashtable;
 
 public class Stopwords {
+	
+	public static void main(String[] args) throws IOException{//也是測試用，應該可刪除
+		Stopwords s = new Stopwords();
+		s.stopword();
+	}
+	
 	public String stopword() throws IOException{
-		//make stopwords dictionary.
+		//讀取stopword.txt製作成hashtable
 		BufferedReader br = new BufferedReader(new FileReader("english.stop.txt"));
 		Hashtable stopwords = new Hashtable();
 		String word;
@@ -15,14 +21,15 @@ public class Stopwords {
 			stopwords.put(word, 0);
 		}
 		
-		String line = "Hello, there are many apples.";
+		String line = "Hello, there are many apples.";//測資
 		String newline = "";
-		String[] token = line.split(" ");
+		String[] token = line.toLowerCase().split(" ");//轉換成小寫後，進行切割，一樣也只切空白，所以可能導致和符號相連的沒辦法過濾
 		for(String e1: token){
 			if(!stopwords.containsKey(e1)){
 				newline += e1 + " ";
 			}
 		}
+		System.out.print(newline);
 		
 		return newline;
 	}
--- src/feature/Tokenization.java
+++ src/feature/Tokenization.java
@@ -1,21 +1,29 @@
 package feature;
 import edu.stanford.nlp.process.*;
+import main.Main;
 public class Tokenization{
+	
+	public static void main(String[] args){//這裡是為了test而加的main，應該可以刪除
+		Tokenization t = new Tokenization();
+		t.tokenize();
+	}
+	
 	public String tokenize() {
 		
-		String test = "Hello, There are many apples.";
-		String content = test;
-		//content = content.split(",")[3];
-		String[] content_token = content.split(" ");
+		String test = "Hello, there are many apples.";//測資
+		String content = test;//到時候讀取的content
+		String[] content_token = content.split(" ");//只切空白，和逗號句號相連的判斷不到。
+		content = "";
 		for(String e1: content_token){
-			String stemming = new Morphology().stem(e1);
+			String stemming = new Morphology().stem(e1);//stemming
+			content += stemming + " ";//stem後重組
 		}
+		System.out.print(content);
 		
 		for(String e2: content_token){
-			String lemma = new Morphology().lemma(e2, "VBG");//tag?
+			//VBG -> 指定VBG變成原型，跟stem用法相同，基本上用不到。
+			String lemma = new Morphology().lemma(e2, "VBG");
 		}
-		//stopword
-		
 		return test;
 	}
 }
