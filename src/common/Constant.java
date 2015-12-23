412c0df91d825d13a7e87c3025360a0fb38f5fd6
連振安 <john821216@gmail.com>
2015-12-24 00:15:57 +0800

    add gui

--- src/common/Constant.java
+++ src/common/Constant.java
@@ -11,4 +11,22 @@ public class Constant {
 	public static HashMap<String, Boolean> stopwords= new HashMap<String, Boolean>();
 	public static String[] punctuation = {",",".","!","_","+","=","(",")","@","#","'","\"",":","?"};
 	public static HashMap<Word, String> lexicon = new HashMap<Word,String>();
+	public static String FilePath;
+	
+	
+	//default, lexicon, improve
+	public static String rawDataProcess="";
+	
+	public static boolean removePun = false;
+	public static boolean removeUnMeaning = false;
+	public static boolean stem = false;
+	public static boolean negation = false;
+	
+	//navie, me
+	public static String classifier="";
+	
+	//uni, bi, unibi, unipo ,no
+	public static String classifierFeature=""; 
+	
+	
 }
